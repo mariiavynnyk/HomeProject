c7bea940b49dfaf5ad8b07dc70fab64e9320ebd6
test <test@MacRaner.local>
2021-11-27 22:09:45 +0200

    minor changes

--- scripts/clear.sh
+++ scripts/clear.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-docker container stop selenoid-ui selenoid
-docker container rm selenoid-ui selenoid
-docker rmi $(docker images -q selenoid/vnc)
-docker rmi $(docker images -q aerokube/selenoid)
-docker rmi $(docker images -q aerokube/selenoid-ui)
-docker rmi $(docker images -q maven)
-docker network rm selenoid
+set -e
+EXIT_CODE=0
+
+docker container stop selenoid-ui selenoid || EXIT_CODE=$?
+docker container rm selenoid-ui selenoid || EXIT_CODE=$?
+docker rmi $(docker images -q selenoid/vnc) || EXIT_CODE=$?
+docker rmi $(docker images -q aerokube/selenoid) || EXIT_CODE=$?
+docker rmi $(docker images -q aerokube/selenoid-ui) || EXIT_CODE=$?
+docker rmi $(docker images -q maven) || EXIT_CODE=$?
+docker network rm selenoid || EXIT_CODE=$?
+
+echo $EXIT_CODE
